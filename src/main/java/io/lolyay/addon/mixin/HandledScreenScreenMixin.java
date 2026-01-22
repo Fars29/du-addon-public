c7fb5920488f7785b0fcde2bf27274675da0b0c8
LOLYAY <lolyay@lolyay.de>
2026-01-22 18:23:47 +0100

    Remove outdated commands and modules (AntiSetServerPosition, SuperReach, ForwardClipCommand, TpCommand), fix slot validation in HandledScreenScreenMixin, and enhance ClickSlotCommand.

--- src/main/java/io/lolyay/addon/mixin/HandledScreenScreenMixin.java
+++ src/main/java/io/lolyay/addon/mixin/HandledScreenScreenMixin.java
@@ -6,7 +6,6 @@ import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtHelper;
@@ -43,6 +42,10 @@ public abstract class HandledScreenScreenMixin<T extends ScreenHandler> extends 
                 .onEnter(txt -> {
                     try {
                         int x = Integer.parseInt(txt.getText());
+                        if (screenHandler.getInventory().getStack(x) == null || screenHandler.getInventory().getStack(x).getRegistryEntry().getIdAsString().equals("minecraft:air")) {
+                            ChatUtils.error("Slot doesnt contain anything!");
+                            return;
+                        }
                         NbtElement element = ItemStack.CODEC.encodeStart(NbtOps.INSTANCE, screenHandler.getInventory().getStack(x)).result().get();
                         ChatUtils.sendMsg(NbtHelper.toPrettyPrintedText(element));
                         mc.keyboard.setClipboard(NbtHelper.toPrettyPrintedText(element).getString());
