3d6b63deb820b296a5915bef5fa614d3e22eaee7
LOLYAY <lolyay@lolyay.de>
2026-01-22 18:23:47 +0100

    Remove outdated commands and modules (AntiSetServerPosition, SuperReach, ForwardClipCommand, TpCommand), fix slot validation in HandledScreenScreenMixin, and enhance ClickSlotCommand.

--- src/main/java/io/lolyay/addon/commands/ClickSlotCommand.java
+++ src/main/java/io/lolyay/addon/commands/ClickSlotCommand.java
@@ -17,7 +17,9 @@ public class ClickSlotCommand extends Command {
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.then(argument("slot", IntegerArgumentType.integer())
                .then(argument("button", IntegerArgumentType.integer())
-               .then(argument("action", EnumArgumentType.enumArgument(SlotActionType.PICKUP))
+
+
+                       .then(argument("action", EnumArgumentType.enumArgument(SlotActionType.PICKUP))
                .executes((context) -> {
                    assert mc.interactionManager != null; // Again, you shouldn't be able to run commands if you're not in a game
                    int slot = context.getArgument("slot", Integer.class);
@@ -26,6 +28,26 @@ public class ClickSlotCommand extends Command {
                    int syncId = mc.currentScreen instanceof GenericContainerScreen screen ? screen.getScreenHandler().syncId : 0;
                    mc.interactionManager.clickSlot(syncId, slot, button, action, mc.player);
                    return SINGLE_SUCCESS;
-               }))));
+               }))
+
+                       .then(argument("times", IntegerArgumentType.integer())
+                           .then(argument("type", IntegerArgumentType.integer(0, SlotActionType.values().length - 1))
+                               .executes(context -> {
+                                   assert mc.interactionManager != null; // Again, you shouldn't be able to run commands if you're not in a game
+                                   int slot = context.getArgument("slot", Integer.class);
+                                   int button = context.getArgument("button", Integer.class);
+                                   SlotActionType action = SlotActionType.values()[context.getArgument("type", Integer.class)];
+                                   int times = context.getArgument("times", Integer.class);
+                                   for (int i = 0; i < times; i++) {
+                                       int syncId = mc.currentScreen instanceof GenericContainerScreen screen ? screen.getScreenHandler().syncId : 0;
+                                       mc.interactionManager.clickSlot(syncId, slot, button, action, mc.player);
+                                   }
+
+                                   return SINGLE_SUCCESS;
+                               })
+                           ))
+
+
+               ));
     }
 }
