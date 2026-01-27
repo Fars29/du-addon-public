565a3ad9970c5fddf29af8bd95e9cc85a2239918
LOLYAY <lolyay@lolyay.de>
2026-01-27 16:40:14 +0100

    Fix error .clickslot not working in shulker GUIs

--- src/main/java/io/lolyay/addon/commands/ClickSlotCommand.java
+++ src/main/java/io/lolyay/addon/commands/ClickSlotCommand.java
@@ -4,7 +4,7 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import io.lolyay.addon.arguments.EnumArgumentType;
 import meteordevelopment.meteorclient.commands.Command;
-import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.command.CommandSource;
 import net.minecraft.screen.slot.SlotActionType;
 
@@ -17,15 +17,13 @@ public class ClickSlotCommand extends Command {
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.then(argument("slot", IntegerArgumentType.integer())
                .then(argument("button", IntegerArgumentType.integer())
-
-
                        .then(argument("action", EnumArgumentType.enumArgument(SlotActionType.PICKUP))
                .executes((context) -> {
                    assert mc.interactionManager != null; // Again, you shouldn't be able to run commands if you're not in a game
                    int slot = context.getArgument("slot", Integer.class);
                    int button = context.getArgument("button", Integer.class);
                    SlotActionType action = context.getArgument("action", SlotActionType.class);
-                   int syncId = mc.currentScreen instanceof GenericContainerScreen screen ? screen.getScreenHandler().syncId : 0;
+                   int syncId = mc.currentScreen instanceof HandledScreen<?> screen ? screen.getScreenHandler().syncId : 0;
                    mc.interactionManager.clickSlot(syncId, slot, button, action, mc.player);
                    return SINGLE_SUCCESS;
                }))
@@ -39,7 +37,7 @@ public class ClickSlotCommand extends Command {
                                    SlotActionType action = SlotActionType.values()[context.getArgument("type", Integer.class)];
                                    int times = context.getArgument("times", Integer.class);
                                    for (int i = 0; i < times; i++) {
-                                       int syncId = mc.currentScreen instanceof GenericContainerScreen screen ? screen.getScreenHandler().syncId : 0;
+                                       int syncId = mc.currentScreen instanceof HandledScreen<?> screen ? screen.getScreenHandler().syncId : 0;
                                        mc.interactionManager.clickSlot(syncId, slot, button, action, mc.player);
                                    }
 
